b03682e96c769317d50bdc6d6dd89de032e29d23
Alan Holt <aholt@penlink.com>
2021-01-25 16:44:50 -0600

    initial commit including reproduction of behavior

---
+++ AuthStateProvider.OwningCompBase.Bug/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthStateProvider.OwningCompBase.Bug
+{
+    public interface IUserService
+    {
+        Task<string> GetUserNameAsync();
+    }
+}
---
+++ AuthStateProvider.OwningCompBase.Bug/UserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace AuthStateProvider.OwningCompBase.Bug
+{
+    public class UserService : IUserService
+    {
+        private readonly AuthenticationStateProvider _AuthStateProvider;
+
+        public UserService(AuthenticationStateProvider authStateProvider)
+        {
+            _AuthStateProvider = authStateProvider;
+        }
+
+        public async Task<String> GetUserNameAsync()
+        {
+            AuthenticationState authState = await _AuthStateProvider.GetAuthenticationStateAsync();
+            return authState.User.Identity.Name ?? string.Empty;
+        }
+    }
+}
